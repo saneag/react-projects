51e292a31d9a2076280553eb9d6e65f9f74d1c6e
saneag <garstea2002@mail.ru>
2022-06-07 18:37:54 +0300

    added cards with info and stylized them

--- cryptocurrency/src/components/Cards.jsx
+++ cryptocurrency/src/components/Cards.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom'
 
 function Cards({ image, title, text, button }) {
     return (
-        <div className='cards_container'>
-
+        <div className='card'>
+            <img src={image} width='80px' />
+            <p className='cards_title'>{title}</p>
+            <p className='cards_text'>{text}</p>
+            <Link to="/" className='cards_button'>{button} <FontAwesomeIcon icon={faArrowRight} /> </Link>
         </div>
     )
 }
--- cryptocurrency/src/pages/Home.jsx
+++ cryptocurrency/src/pages/Home.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 
+import Cards from '../components/Cards'
+
+import trade_desk from '../assets/img/trade_desk.png'
+import coinFlip_ATMs from '../assets/img/coinflip_atms.png'
+import CoinFlip_Wallet from '../assets/img/coinflip_wallet.png'
+
 function Home() {
     return (
         <div className='container'>
@@ -9,9 +15,16 @@ function Home() {
                 <p>Buy, sell, and grow your crypto with CoinFlip, the platform dedicated to every trader at every level.</p>
                 <button className='get_started_btn'>Get Started</button>
             </section>
-            <section id='second_section'>
 
+            <section id='second_section'>
+                <div className='cards_container'>
+                    <Cards image={trade_desk} title='Trade Desk' text='Invest in crypto anytime, anywhere with our safe, secure, and easy to use online platform' button='Get Started' />
+                    <Cards image={coinFlip_ATMs} title='CoinFlip ATMs' text='We have thousands of ATMs located across the U.S. where you can easily convert cash to crypto' button='Find an ATM' />
+                    <Cards image={CoinFlip_Wallet} title='CoinFlip Wallet' text='Store your growing investments in our
+                non-custodial wallet that gives you access to a full suite of DeFi services in one place' button='Download the App' />
+                </div>
             </section>
+
         </div>
     )
 }
