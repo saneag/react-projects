0627ec1bb63068a771772df80b9716fbfc7f954f
saneag <garstea2002@mail.ru>
2022-06-10 17:46:01 +0300

    edited classname

--- cryptocurrency/src/components/Section_1.jsx
+++ cryptocurrency/src/components/Section_1.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Section_1() {
     return (
-        <section id='text_block'>
+        <section className='text_block'>
             <p>We make crypto clear and simple</p>
             <p>Buy, sell, and grow your crypto with CoinFlip, the platform dedicated to every trader at every level.</p>
             <button className='get_started_btn'>Get Started</button>
