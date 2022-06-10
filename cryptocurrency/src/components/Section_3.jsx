9875c337b442f5be4d33a8a02274c98b92af3d64
saneag <garstea2002@mail.ru>
2022-06-10 17:38:53 +0300

    updated styles

--- cryptocurrency/src/components/Section_3.jsx
+++ cryptocurrency/src/components/Section_3.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Section_3() {
     return (
-        <section style={{ 'min-height': '400px', 'margin-top': '100px' }} id='text_block'>
+        <section style={{ minHeight: '400px', marginTop: '100px' }} id='text_block'>
             <p className='crypto_caption'>A crypto investment platform that invests in you</p>
             <p>We invest more resources than any other platrom in making sure great support from real people is a click away, whenever you need it.</p>
             <button className='get_started_btn'>Get Started</button>
