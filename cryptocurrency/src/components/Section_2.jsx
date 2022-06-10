ff5d1baad9ef07fb0c4bcc4807a6ac3ca4564fb3
saneag <garstea2002@mail.ru>
2022-06-10 20:37:27 +0300

    edited cards display and json file

--- cryptocurrency/src/components/Section_2.jsx
+++ cryptocurrency/src/components/Section_2.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 
 import Cards from './Cards'
-import cards from '../assets/cards.json'
+import cards from '../assets/json/cards.json'
 
-import trade_desk from '../assets/img/trade_desk.png'
-import coinFlip_ATMs from '../assets/img/coinflip_atms.png'
-import CoinFlip_Wallet from '../assets/img/coinflip_wallet.png'
+import trade_desk from '../assets/img/cards/trade_desk.png'
+import coinFlip_ATMs from '../assets/img/cards/coinflip_atms.png'
+import CoinFlip_Wallet from '../assets/img/cards/coinflip_wallet.png'
 
 function Section_2() {
+    const images = [trade_desk, coinFlip_ATMs, CoinFlip_Wallet]
+
     return (
         <section id='cards_block'>
             <div className='cards_container'>
-                <Cards image={trade_desk} title={cards.trade_desk.title} text={cards.trade_desk.text} button={cards.trade_desk.button} />
-                <Cards image={coinFlip_ATMs} title={cards.coinFlip_ATMs.title} text={cards.coinFlip_ATMs.text} button={cards.coinFlip_ATMs.button} />
-                <Cards image={CoinFlip_Wallet} title={cards.CoinFlip_Wallet.title} text={cards.CoinFlip_Wallet.text} button={cards.CoinFlip_Wallet.button} />
+                {
+                    cards.map((card, index) => <Cards key={card.title} image={images[index]} {...card} />)
+                }
             </div>
         </section>
     )
