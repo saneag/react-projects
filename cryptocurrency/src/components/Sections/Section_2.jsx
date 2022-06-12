aa880efabc140aa561eac4316c53f67a95b95cb5
saneag <garstea2002@mail.ru>
2022-06-13 00:17:50 +0300

    added db

--- cryptocurrency/src/components/Sections/Section_2.jsx
+++ cryptocurrency/src/components/Sections/Section_2.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 
 import Cards from '../Cards'
-import cards from '../../assets/json/cards.json'
 
 import trade_desk from '../../assets/img/cards/trade_desk.png'
 import coinFlip_ATMs from '../../assets/img/cards/coinflip_atms.png'
 import CoinFlip_Wallet from '../../assets/img/cards/coinflip_wallet.png'
 
 function Section_2() {
+
+    const [cards, setCards] = React.useState([])
+
+    React.useEffect(() => {
+        fetch('https://62a36f1d21232ff9b21fe3d5.mockapi.io/cards')
+            .then(res => res.json())
+            .then(cards => {
+                setCards(cards)
+            })
+    }, [])
+
     const images = [trade_desk, coinFlip_ATMs, CoinFlip_Wallet]
 
     return (
