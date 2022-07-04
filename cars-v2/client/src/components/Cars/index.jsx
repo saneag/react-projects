48c2646205714031530ba2230547a091eca150a0
A13x <69859268+saneag@users.noreply.github.com>
2022-07-04 13:58:04 +0300

    added image compression but only for test

--- cars-v2/client/src/components/Cars/index.jsx
+++ cars-v2/client/src/components/Cars/index.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
-import { motion, LazyMotion, domAnimation } from 'framer-motion'
+import { motion } from 'framer-motion'
+import imageCompression from 'browser-image-compression'
 import { ShowModalCar } from '../../pages/Home'
 
 import styles from './styles.module.scss'
 
 function Car(car) {
     const { setSelectedImg } = React.useContext(ShowModalCar)
+
+    //image compression
+    // async function handleImageUpload(event) {
+
+    //     const imageFile = event.target.files[0];
+    //     console.log('originalFile instanceof Blob', imageFile instanceof Blob); // true
+    //     console.log(`originalFile size ${imageFile.size / 1024 / 1024} MB`);
+
+    //     const options = {
+    //         maxSizeMB: 1,
+    //         maxWidthOrHeight: 1920,
+    //         useWebWorker: true
+    //     }
+    //     try {
+    //         const compressedFile = await imageCompression(imageFile, options);
+    //         console.log('compressedFile instanceof Blob', compressedFile instanceof Blob); // true
+    //         console.log(`compressedFile size ${compressedFile.size / 1024 / 1024} MB`); // smaller than maxSizeMB
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    // }
 
     return (
         <div className={styles.root}>
@@ -15,13 +37,12 @@ function Car(car) {
                 animate={{ opacity: 1 }}
                 transition={{ duration: 1 }}
             >
-                <LazyMotion features={domAnimation}>
-                    <motion.img src={car.link} className={styles.image}
-                        animate={{ opacity: 1 }}></motion.img>
-                </LazyMotion>
+                <motion.img src={car.link} className={styles.image}
+                    animate={{ opacity: 1 }}></motion.img>
                 <p>{car.marca} <span className={styles.model}>{car.model}</span></p>
             </motion.div>
         </div>
+        // <input type="file" accept="image/*" onChange={(e) => handleImageUpload(e)} />
     )
 }
 
