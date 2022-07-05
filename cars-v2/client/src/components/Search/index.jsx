4e49e730ce7680266747b731d40914193164e089
saneag <garstea2002@mail.ru>
2022-07-05 19:45:32 +0300

    added search input and stylized it

--- cars-v2/client/src/components/Search/index.jsx
+++ cars-v2/client/src/components/Search/index.jsx
@@ -3,20 +3,10 @@ import React from 'react'
 import styles from './styles.module.scss'
 
 function Search() {
-    const input_value = ['Marca', 'Model', 'Din anul', 'Pana in anul',
-        'Capacitatea de la (ml)', 'Capacitatea pana la (ml)', 'Cai putere de la',
-        'Cai putere pana la', 'Pret de la ($)', 'Pret pana la ($)']
     return (
-        <div className={styles.search_inputs}>
-            {input_value.map((value, index) => <Input_Fields key={index} placeholder={value} />)}
+        <div className={styles.search_input}>
+            <input placeholder='Cars search ...' />
         </div>
     )
 }
-
-function Input_Fields({ placeholder }) {
-    return (
-        <input value={placeholder} />
-    )
-}
-
 export default Search
