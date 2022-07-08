fc18a546afd7fce2be389107fbed70a056efef45
saneag <garstea2002@mail.ru>
2022-07-08 20:15:51 +0300

    added sort component

--- cars-v2/client/src/components/Sort/index.jsx
+++ cars-v2/client/src/components/Sort/index.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
+import styles from './styles.module.scss'
+
 function Sort_Cars() {
+    const sortMethods = ['A-Z', 'Z-A', 'Lowest to highest',
+        'Highest to lowest', 'Newest to oldest', 'Oldest to newest']
     return (
-        <></>
+        <div className={styles.sort_div}>
+            <div className={styles.sort_title}>Sort by:</div>
+            <div className={styles.sort_options}>
+                {sortMethods.map((method, index) => <div key={index} className={styles.sort_option}>{method}</div>)}
+            </div>
+        </div>
     )
 }
 
