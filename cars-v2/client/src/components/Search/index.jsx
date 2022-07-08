533dd3d6a3067e51de2481490daeff2a46f87e70
saneag <garstea2002@mail.ru>
2022-07-08 20:13:32 +0300

    modified search components and made functionality

--- cars-v2/client/src/components/Search/index.jsx
+++ cars-v2/client/src/components/Search/index.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { SearchContext } from '../../pages/Home'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch, faX } from '@fortawesome/free-solid-svg-icons'
 
 import styles from './styles.module.scss'
 
 function Search() {
+    const { search, setSearch } = React.useContext(SearchContext)
     return (
         <div className={styles.search_input}>
-            <input placeholder='Cars search ...' />
+            <div>
+                <FontAwesomeIcon className={`search_icon ${styles.seach_icon}`} icon={faSearch} />
+                <input className='search_input' value={search} onChange={e => setSearch(e.target.value)} placeholder='Cars search ...' />
+                {search && <FontAwesomeIcon className={`delete_icon ${styles.delete_icon}`} icon={faX} onClick={() => setSearch('')} />}
+            </div>
         </div>
     )
 }
