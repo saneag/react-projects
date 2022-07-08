0b99d8186826df971add75f9ffccfd83dd154b05
saneag <garstea2002@mail.ru>
2022-07-08 20:16:19 +0300

    added sort component

--- cars-v2/client/src/pages/Home.jsx
+++ cars-v2/client/src/pages/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 import Cars from '../components/Cars'
 import Search from '../components/Search'
+import Sort from '../components/Sort'
 import Modal from '../components/Modal'
 import Pagination from '../components/Pagination'
 import Skeleton from '../components/Skeleton'
 
 export const ShowModalCar = React.createContext(null)
+export const SearchContext = React.createContext(null)
 
 function Home() {
     const [cars, setCars] = React.useState([])
@@ -16,10 +18,11 @@ function Home() {
     const [page, setPage] = React.useState(1)
     const [carsLimit, setCarsLimit] = React.useState(12)
     const [showReadMore, setShowReadMore] = React.useState(true)
+    const [search, setSearch] = React.useState('')
 
     React.useEffect(() => {
         setLoading(true)
-        fetch(`https://62a36f1d21232ff9b21fe3d5.mockapi.io/cars?page=${page}&limit=${carsLimit}`)
+        fetch(`https://62a36f1d21232ff9b21fe3d5.mockapi.io/cars?page=${page}&limit=${carsLimit}&search=${search}`)
             .then(res => res.json())
             .then(data => {
                 setLoading(false)
@@ -29,7 +32,7 @@ function Home() {
                 }
                 setShowReadMore(carsLimit > data.length ? false : true)
             })
-    }, [page, carsLimit])
+    }, [page, carsLimit, search])
 
     const showMoreCars = () => {
         if (carsLimit <= cars.length) {
@@ -40,7 +43,12 @@ function Home() {
     return (
         <ShowModalCar.Provider value={{ selectedImg, setSelectedImg }}>
             <main>
-                <Search />
+                <div className='top_options'>
+                    <SearchContext.Provider value={{ search, setSearch }}>
+                        <Search />
+                    </SearchContext.Provider>
+                    <Sort />
+                </div>
                 <div className="cars_gallery">
                     {
                         loading ? [...Array(carsLimit)].map((_, index) => <Skeleton key={index} />) :
