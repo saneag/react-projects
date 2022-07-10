0a1f6560e5e96250011c2aba6b70097651d0d28f
saneag <garstea2002@mail.ru>
2022-07-10 15:14:01 +0300

    added redux to components and connected them together

--- cars-v2/client/src/pages/Home.jsx
+++ cars-v2/client/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 import Cars from '../components/Cars'
 import Search from '../components/Search'
@@ -7,26 +8,22 @@ import Modal from '../components/Modal'
 import Pagination from '../components/Pagination'
 import Skeleton from '../components/Skeleton'
 
-export const ShowModalCar = React.createContext(null)
-export const SearchContext = React.createContext(null)
-export const SortCarsContext = React.createContext(null)
-export const SortOrderContext = React.createContext(null)
-
 function Home() {
+    console.log('updated')
+    const sortBy = useSelector(state => state.sort.sortBy)
+    const sortOrder = useSelector(state => state.sort.sortOrder)
+    const search = useSelector(state => state.search.search)
+    const selectedImg = useSelector(state => state.showModalCar.selectedImg)
+
     const [cars, setCars] = React.useState([])
-    const [selectedImg, setSelectedImg] = React.useState(null)
-    const [showModal, setShowModal] = React.useState(false)
     const [loading, setLoading] = React.useState(true)
     const [page, setPage] = React.useState(1)
     const [carsLimit, setCarsLimit] = React.useState(12)
     const [showReadMore, setShowReadMore] = React.useState(true)
-    const [search, setSearch] = React.useState('')
-    const [sortCars, setSortCars] = React.useState('marca')
-    const [sortOrder, setSortOrder] = React.useState(true)
 
     React.useEffect(() => {
         setLoading(true)
-        fetch(`https://62a36f1d21232ff9b21fe3d5.mockapi.io/cars?sortBy=${sortCars}&order=${sortOrder ? 'desc' : 'asc'}&&page=${page}&limit=${carsLimit}&search=${search}`)
+        fetch(`https://62a36f1d21232ff9b21fe3d5.mockapi.io/cars?sortBy=${sortBy}&order=${sortOrder ? 'desc' : 'asc'}&&page=${page}&limit=${carsLimit}&search=${search}`)
             .then(res => res.json())
             .then(data => {
                 setLoading(false)
@@ -36,7 +33,7 @@ function Home() {
                 }
                 setShowReadMore(carsLimit > data.length ? false : true)
             })
-    }, [page, carsLimit, search, sortCars, sortOrder])
+    }, [page, carsLimit, search, sortBy, sortOrder])
 
     const showMoreCars = () => {
         if (carsLimit <= cars.length) {
@@ -44,39 +41,31 @@ function Home() {
         }
     }
 
-
     return (
-        <ShowModalCar.Provider value={{ selectedImg, setSelectedImg }}>
-            <main>
-                <div className='top_options'>
-                    <SearchContext.Provider value={{ search, setSearch }}>
-                        <Search />
-                    </SearchContext.Provider>
-                    <SortCarsContext.Provider value={{ sortCars, setSortCars }}>
-                        <SortOrderContext.Provider value={{ sortOrder, setSortOrder }}>
-                            <Sort />
-                        </SortOrderContext.Provider>
-                    </SortCarsContext.Provider>
-                </div>
-                <div className="cars_gallery">
-                    {
-                        loading ? [...Array(carsLimit)].map((_, index) => <Skeleton key={index} />) :
-                            cars.map(car => <Cars key={car.marca + car.model + car.pret} {...car} />)
-                    }
-                </div>
+        <main>
+            <div className='top_options'>
+                <Search />
+                <Sort />
+            </div>
+            <div className="cars_gallery">
                 {
-                    selectedImg && (
-                        <Modal cars={cars} showModal={showModal} setShowModal={setShowModal} />
-                    )
+                    loading ? [...Array(carsLimit)].map((_, index) => <Skeleton key={index} />) :
+                        cars.map(car => <Cars key={car.marca + car.model + car.pret} {...car} />)
                 }
-                <Pagination onChangePage={number => setPage(number)} />
-                <div className='showMore'>
-                    {
-                        showReadMore && page === 1 && <button className='showMoreBtn' onClick={showMoreCars}>Show more</button>
-                    }
-                </div>
-            </main>
-        </ShowModalCar.Provider>
+            </div>
+            {
+                selectedImg && (
+                    <Modal cars={cars} />
+                )
+            }
+            <Pagination onChangePage={number => setPage(number)} />
+            <div className='showMore'>
+                {
+                    showReadMore && page === 1 &&
+                    <button className='showMoreBtn' onClick={showMoreCars}>Show more</button>
+                }
+            </div>
+        </main>
     )
 }
 
