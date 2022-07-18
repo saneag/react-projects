f34ebaab39ffaa49e2b0975f60c42ee40b2a004a
Saneag <garstea2002@mail.ru>
2022-07-18 23:58:25 +0300

    working on add car page. almost finished

--- cars-v2/client/src/pages/AddCar/index.jsx
+++ cars-v2/client/src/pages/AddCar/index.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { Navigate } from 'react-router-dom'
 import { handleImageUpload } from '../../utils/compress_images'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFileCirclePlus, faXmark } from '@fortawesome/free-solid-svg-icons'
 
 import { isAuthenticated } from '../../redux/slices/userAuthSlice'
 
@@ -10,11 +12,21 @@ import styles from './styles.module.scss'
 function AddCar() {
     const isAuth = useSelector(isAuthenticated)
 
-    // const inputs = ['brand', 'model', 'year', 'fuel type',
-    //     'engine capacity', 'engine power', 'traction type', 'gearbox type',
-    //     'price', 'image url']
+    const initialState = {
+        brand: '',
+        model: '',
+        year: '',
+        fuel_type: '',
+        engine_capacity: '',
+        engine_power: '',
+        traction_type: '',
+        gearbox_type: '',
+        price: '',
+    }
 
-    if (!isAuth) {
+    const [stateOptions, setStateOptions] = React.useState(initialState)
+
+    if (!(window.localStorage.getItem('token') || window.sessionStorage.getItem('token')) && !isAuth) {
         return <Navigate to="/" />
     }
 
@@ -23,10 +35,60 @@ function AddCar() {
         <main className={styles.root}>
             <div className={styles.add_car_form}>
                 <p className={styles.caption}>Add Car</p>
-
+                <form>
+                    <div>
+                        {Object.keys(stateOptions).slice(0, 5).map(key => {
+                            return <ShowInputs
+                                value={key}
+                                stateOptions={stateOptions}
+                                setStateOptions={setStateOptions} />
+                        })}
+                    </div>
+                    <div>
+                        {Object.keys(stateOptions).slice(5, 9).map(key => {
+                            return <ShowInputs
+                                value={key}
+                                stateOptions={stateOptions}
+                                setStateOptions={setStateOptions} />
+                        })}
+                        {/*
+                            TODO: 
+                            Create drag and drop 
+                        */}
+                        <div className={styles.input_field}>
+                            <input id='file_upload' type="file" accept='image/*' onChange={(e) => handleImageUpload(e)} />
+                            <label htmlFor='file_upload' className={styles.file_upload}>
+                                <p>Image URL</p>
+                                <FontAwesomeIcon icon={faFileCirclePlus} />
+                            </label>
+                        </div>
+                    </div>
+                </form>
             </div>
         </main>
     )
 }
+// TODO:
+//Move it to components and fix unique key issue
+function ShowInputs({ value, stateOptions, setStateOptions }) {
+    return (
+        <div key={value} className={styles.input_field}>
+            <label htmlFor={value}>{value.replace(/_/, ' ')}</label>
+            <input
+                value={stateOptions[value]}
+                type={typeof stateOptions[value]}
+                id={value}
+                onChange={e => setStateOptions({ ...stateOptions, [value]: e.target.value })}
+            />
+            {stateOptions[value] &&
+                <FontAwesomeIcon
+                    icon={faXmark}
+                    onClick={() => setStateOptions({ ...stateOptions, [value]: '' })}
+                    className={styles.delete_icon}
+                />
+            }
+        </div>
+    )
+}
 
-export default AddCar
+export default React.memo(AddCar)
