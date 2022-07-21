93bdc89cb68b9f83d5fb6ca4cd6c5b7de80ccc23
saneag <garstea2002@mail.ru>
2022-07-21 16:41:50 +0300

    finished add car page

--- cars-v2/client/src/pages/AddCar/index.jsx
+++ cars-v2/client/src/pages/AddCar/index.jsx
@@ -1,30 +1,96 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 import { useSelector } from 'react-redux'
-import { Navigate } from 'react-router-dom'
-import { handleImageUpload } from '../../utils/compress_images'
+import { useNavigate, Navigate, useParams } from 'react-router-dom'
+import imageCompression from 'browser-image-compression'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFileCirclePlus, faXmark } from '@fortawesome/free-solid-svg-icons'
+
+import axios from '../../utils/axios'
+
+import ShowInputs from '../../components/ShowInputs'
 
 import { isAuthenticated } from '../../redux/slices/userAuthSlice'
 
 import styles from './styles.module.scss'
 
 function AddCar() {
+    const { id } = useParams()
+    const navigate = useNavigate()
     const isAuth = useSelector(isAuthenticated)
 
-    const initialState = {
-        brand: '',
-        model: '',
-        year: '',
-        fuel_type: '',
-        engine_capacity: '',
-        engine_power: '',
-        traction_type: '',
-        gearbox_type: '',
-        price: '',
+    const [brand, setBrand] = React.useState('')
+    const [model, setModel] = React.useState('')
+    const [year, setYear] = React.useState('')
+    const [fuel_type, setFuelType] = React.useState('')
+    const [engine_capacity, setEngineCapacity] = React.useState('')
+    const [engine_power, setEnginePower] = React.useState('')
+    const [traction_type, setTractionType] = React.useState('')
+    const [gearbox_type, setGearboxType] = React.useState('')
+    const [price, setPrice] = React.useState('')
+    const [imageUrl, setImageUrl] = React.useState('')
+
+    // React.useEffect(() => {
+    //     setBrand('Hyundai')
+    //     setModel('Sola')
+    //     setYear(2014)
+    //     setFuelType('Benzina')
+    //     setEngineCapacity(1600)
+    //     setEnginePower(123)
+    //     setTractionType('Fata')
+    //     setGearboxType('Automata')
+    //     setPrice(9868)
+    //     setImageUrl('./uploads/hyundai-solaris-i-sedan-facelift-2014.jpg')
+    // }, [])
+
+    const statesNameArray = ['brand', 'model', 'year', 'fuel_type',
+        'engine_capacity', 'engine_power', 'traction_type', 'gearbox_type', 'price']
+
+    const leftStatesArray = [brand, model, year, fuel_type, engine_capacity]
+    const rightStatesArray = [engine_power, traction_type, gearbox_type, price]
+
+    const leftOptionsArray = [setBrand, setModel, setYear, setFuelType, setEngineCapacity]
+    const rightOptionsArray = [setEnginePower, setTractionType, setGearboxType, setPrice]
+
+    const handleChangeFile = async (e) => {
+        try {
+            const options = {
+                maxSizeMB: 1,
+                maxWidthOrHeight: 1920,
+                useWebWorker: true
+            }
+            const formData = new FormData()
+            try {
+                const compressedFile = await imageCompression(e.target.files[0], options)
+                const file = new File([compressedFile], e.target.files[0].name, { type: e.target.files[0].type })
+                formData.append('image', file)
+            }
+            catch (err) {
+                console.log('something went wrong')
+            }
+            const { data } = await axios.post('/upload', formData)
+            setImageUrl(data.url)
+        }
+        catch (err) {
+            console.log(err)
+        }
     }
 
-    const [stateOptions, setStateOptions] = React.useState(initialState)
+    const handleSubmit = async (e) => {
+        try {
+            const fields = {
+                brand, model, year, fuel_type,
+                engine_capacity, engine_power,
+                traction_type, gearbox_type,
+                price, imageUrl
+            }
+            const { data } = await axios.post('/cars', fields)
+            navigate('/')
+        }
+        catch (err) {
+            console.log(err)
+        }
+    }
 
     if (!(window.localStorage.getItem('token') || window.sessionStorage.getItem('token')) && !isAuth) {
         return <Navigate to="/" />
@@ -37,59 +103,80 @@ function AddCar() {
                 <p className={styles.caption}>Add Car</p>
                 <form>
                     <div>
-                        {Object.keys(stateOptions).slice(0, 5).map(key => {
+                        {leftStatesArray.map((value, index) => {
                             return <ShowInputs
-                                key={key}
-                                value={key}
-                                stateOptions={stateOptions}
-                                setStateOptions={setStateOptions} />
+                                key={statesNameArray[index]}
+                                value={statesNameArray[index]}
+                                states={leftStatesArray[index]}
+                                options={leftOptionsArray[index]} />
                         })}
                     </div>
                     <div>
-                        {Object.keys(stateOptions).slice(5, 9).map(key => {
+                        {rightStatesArray.map((value, index) => {
                             return <ShowInputs
-                                key={key}
-                                value={key}
-                                stateOptions={stateOptions}
-                                setStateOptions={setStateOptions} />
+                                key={statesNameArray[index + 5]}
+                                value={statesNameArray[index + 5]}
+                                states={rightStatesArray[index]}
+                                options={rightOptionsArray[index]} />
                         })}
                         {/*
                             TODO: 
                             Create drag and drop 
                         */}
-                        <div className={styles.input_field}>
-                            <input id='file_upload' type="file" accept='image/*' onChange={(e) => handleImageUpload(e)} />
-                            <label htmlFor='file_upload' className={styles.file_upload}>
-                                <p>Image URL</p>
-                                <FontAwesomeIcon icon={faFileCirclePlus} />
-                            </label>
-                        </div>
+                        {!imageUrl &&
+                            <div className={styles.input_field}>
+                                <input
+                                    id='file_upload'
+                                    type="file"
+                                    accept='image/*'
+                                    onChange={handleChangeFile} />
+                                <label htmlFor='file_upload' className={styles.file_upload}>
+                                    <p>Image URL</p>
+                                    <FontAwesomeIcon icon={faFileCirclePlus} />
+                                </label>
+                            </div>
+                        }
+                        {imageUrl &&
+                            <motion.div
+                                initial={{ opacity: 0 }}
+                                animate={{ opacity: 1 }}
+                                transition={{ duration: 0.5 }}
+                                className={styles.input_field}>
+                                <button
+                                    type='button'
+                                    onClick={handleSubmit}
+                                    className={styles.submit_btn}
+                                >Submit</button>
+                            </motion.div>
+                        }
                     </div>
                 </form>
+                {
+                    imageUrl &&
+                    <motion.div
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{ duration: 0.5 }}
+                        className={styles.uploaded_image}
+                    >
+
+                        <motion.img
+                            src={`http://localhost:5000/${imageUrl}`}
+                            alt='car'
+                            className={styles.car_image}
+                        />
+                        <motion.div
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            transition={{ duration: 0.2 }}
+                            onClick={() => setImageUrl('')}
+                        >
+                            <FontAwesomeIcon icon={faXmark} className={styles.delete} />
+                        </motion.div>
+                    </motion.div>
+                }
             </div>
         </main>
-    )
-}
-// TODO:
-//Move it to components and fix unique key issue
-function ShowInputs({ value, stateOptions, setStateOptions }) {
-    return (
-        <div key={value} className={styles.input_field}>
-            <label htmlFor={value}>{value.replace(/_/, ' ')}</label>
-            <input
-                value={stateOptions[value]}
-                type={typeof stateOptions[value]}
-                id={value}
-                onChange={e => setStateOptions({ ...stateOptions, [value]: e.target.value })}
-            />
-            {stateOptions[value] &&
-                <FontAwesomeIcon
-                    icon={faXmark}
-                    onClick={() => setStateOptions({ ...stateOptions, [value]: '' })}
-                    className={styles.delete_icon}
-                />
-            }
-        </div>
     )
 }
 
